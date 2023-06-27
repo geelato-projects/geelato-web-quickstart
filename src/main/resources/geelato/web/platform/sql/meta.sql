7533694b6a3534482aa9d842979dbe8cd32ef57f
diablo1303 <diabloposeidon@hotmail.com>
2023-06-27 15:04:57 +0800

    表单、字段变更功能优化。

--- src/main/resources/geelato/web/platform/sql/meta.sql
+++ src/main/resources/geelato/web/platform/sql/meta.sql
@@ -1,66 +1,85 @@
 -- 模型变更，表名变更
 -- @sql metaResetOrDeleteTable
-UPDATE platform_dev_column
-SET table_name = '$.newEntityName' , del_status = $.delStatus , enable_status = $.enableStatus , column_comment = CONCAT('$.remark',column_comment)
-WHERE table_name = '$.entityName';
-UPDATE platform_dev_table_foreign
-SET main_table = '$.newEntityName' , del_status = $.delStatus , enable_status = $.enableStatus , description = CONCAT('$.remark',description)
-WHERE main_table = '$.entityName';
+UPDATE platform_dev_column SET table_name = '$.newEntityName' , del_status = $.delStatus , enable_status = $.enableStatus WHERE table_name = '$.entityName';
+UPDATE platform_dev_table_foreign SET main_table = '$.newEntityName' , del_status = $.delStatus , enable_status = $.enableStatus WHERE main_table = '$.entityName';
 UPDATE platform_dev_table_foreign SET foreign_table = '$.newEntityName' WHERE foreign_table = '$.entityName';
-UPDATE platform_dev_view
-SET entity_name = '$.newEntityName' , del_status = $.delStatus , enable_status = $.enableStatus , description = CONCAT('$.remark',description)
-WHERE entity_name = '$.entityName';
+UPDATE platform_dev_view SET entity_name = '$.newEntityName' , del_status = $.delStatus , enable_status = $.enableStatus WHERE entity_name = '$.entityName';
 @if $.isTable
-ALTER TABLE $.entityName COMMENT = '$.newComment';
-RENAME TABLE $.entityName TO $.newEntityName;
+    ALTER TABLE $.entityName COMMENT = '$.newComment';
+    RENAME TABLE $.entityName TO $.newEntityName;
 @/if
 
 -- 模型变更，字段变更
 -- @sql metaDeleteColumn
 UPDATE platform_dev_table_foreign
 SET del_status = $.delStatus , enable_status = $.enableStatus , description = CONCAT('$.remark',description)
-WHERE FIND_IN_SET('$.columnName',main_table_col) OR FIND_IN_SET('$.columnName',foreign_table_col);
+WHERE FIND_IN_SET('$.name',main_table_col) OR FIND_IN_SET('$.name',foreign_table_col);
+@if $.isColumn
+    alter table $.tableName CHANGE COLUMN `$.name` `$.newName` $.type
+        @if !$.nullable
+        not null
+        @/if
+        @if $.defaultValue!='' && $.defaultValue!=null
+        @if $.dataType=='BIT'
+        DEFAULT $.defaultValue
+        @/if
+        @if $.dataType!='BIT'
+        DEFAULT '$.defaultValue'
+        @/if
+        @/if
+        @if $.autoIncrement
+        AUTO_INCREMENT
+        @/if
+        @if $.comment!='' && $.comment!=null
+        COMMENT '$.comment'
+        @/if
+    ;
+@/if
 
 -- 模型变更，字段变更
 -- @sql metaResetColumn
 UPDATE platform_dev_table_foreign SET main_table_col = '$.formname' WHERE FIND_IN_SET('$.modelname',main_table_col);
 UPDATE platform_dev_table_foreign SET foreign_table_col = '$.formname' WHERE FIND_IN_SET('$.modelname',foreign_table_col);
-alter table $.modeltableName CHANGE COLUMN `$.modelname` `$.formname` $.formtype
-    @if !$.formnullable
+@if $.isColumn
+    alter table $.modeltableName CHANGE COLUMN `$.modelname` `$.formname` $.formtype
+        @if !$.formnullable
         not null
-    @/if
-    @if $.formdefaultValue!='' && $.formdefaultValue!=null
+        @/if
+        @if $.formdefaultValue!='' && $.formdefaultValue!=null
         @if $.formdataType=='BIT'
-            DEFAULT $.formdefaultValue
+        DEFAULT $.formdefaultValue
         @/if
         @if $.formdataType!='BIT'
-            DEFAULT '$.formdefaultValue'
+        DEFAULT '$.formdefaultValue'
         @/if
-    @/if
-    @if $.formautoIncrement
+        @/if
+        @if $.formautoIncrement
         AUTO_INCREMENT
-    @/if
-    @if $.formcomment!='' && $.formcomment!=null
+        @/if
+        @if $.formcomment!='' && $.formcomment!=null
         COMMENT '$.formcomment'
-    @/if
-;
-alter table $.modeltableName add $.modelname $.modeltype
-  @if !$.modelnullable
-    not null
-  @/if
-  @if $.modeldefaultValue!='' && $.modeldefaultValue!=null
-    @if $.modeldataType=='BIT'
-      DEFAULT $.modeldefaultValue
-    @/if
-    @if $.modeldataType!='BIT'
-      DEFAULT '$.modeldefaultValue'
-    @/if
-  @/if
-  @if $.modelautoIncrement
-     AUTO_INCREMENT
-  @/if
-  @if $.modelcomment!='' && $.modelcomment!=null
-    COMMENT '$.modelcomment'
-  @/if
-;
-UPDATE $.modeltableName set $.modelname = $.formname where 1=1;
+        @/if
+    ;
+@/if
+@if $.isCopy
+    alter table $.modeltableName add $.modelname $.modeltype
+      @if !$.modelnullable
+        not null
+      @/if
+      @if $.modeldefaultValue!='' && $.modeldefaultValue!=null
+        @if $.modeldataType=='BIT'
+          DEFAULT $.modeldefaultValue
+        @/if
+        @if $.modeldataType!='BIT'
+          DEFAULT '$.modeldefaultValue'
+        @/if
+      @/if
+      @if $.modelautoIncrement
+         AUTO_INCREMENT
+      @/if
+      @if $.modelcomment!='' && $.modelcomment!=null
+        COMMENT '$.modelcomment'
+      @/if
+    ;
+    UPDATE $.modeltableName set $.modelname = $.formname where 1=1;
+@/if
